2dd90f32178f7a16bc245df42af64ef8d505d4da
Richard Cook <rcook@rcook.org>
2014-06-22 19:27:48 -0700

    Parse --debug switch from command line

--- src/sqexec/main.cpp
+++ src/sqexec/main.cpp
@@ -6,40 +6,29 @@ using namespace std;
 #include "libsqasm/assembler.hpp"
 #include "libsqrun/runner.hpp"
 
-static void assemble(const string &fileName, const bool debug = false)
+static void assemble(const string &fileName, const bool debug)
 {
     auto compilationData(Assembler::assemble(fileName));
-    Runner::run(compilationData->ops());
-
-    if (debug)
-    {
-        if (!compilationData->symbolTable().empty())
-        {
-            cout << "Symbol table" << endl;
-            for (auto &pair : compilationData->symbolTable())
-            {
-                cout << "  symbol: " << pair.first << " = " << pair.second << endl;
-            }
-        }
-
-        if (!compilationData->cells().empty())
-        {
-            cout << "Cells" << endl;
-            for (auto &cell : compilationData->cells())
-            {
-                cout << "  cell: " << cell->toString() << endl;
-            }
-        }
-    }
+    Runner::run(compilationData->ops(), debug);
 }
 
 int main(int argc, char *argv[])
 {
     try
     {
+        bool debug = false;
+
         for (auto i = 1; i < argc; ++i)
         {
-            assemble(argv[i]);
+            string arg(argv[i]);
+
+            if (i == 1 && arg.compare("--debug") == 0)
+            {
+                debug = true;
+                continue;
+            }
+
+            assemble(arg, debug);
         }
         return EXIT_SUCCESS;
     }
